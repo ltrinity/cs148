0ebac037664fd600a2cae9406e7c7d5ac6cd62f9
Luke Trinity <ltrinity@uvm.edu>
2016-10-28 15:09:39 -0400

    resetting

---
+++ sitemap.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CS 148 Database Design Webpage</title>
+        <meta name="author" content="Luke Trinity">
+        <meta name="description" content="navigate to lab pages">
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+<p>Develop <a href="cs148_develop/labs/lab1/index.php">Lab1</a></p>
+<p>Develop <a href="cs148_develop/labs/lab2/index.php">Lab2</a></p>
+<p>Develop <a href="cs148_develop/labs/lab3/index.php">Lab3</a></p>
+<p>Develop <a href="cs148_develop/labs/lab4/index.php">Lab4</a></p>
+<p>Develop <a href="cs148_develop/labs/lab5/er.php">Lab5 aka er diagram</a></p>
+
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab1/index.php">Lab1</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab2/index.php">Lab2</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab3/index.php">Lab3</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab4/index.php">Lab4</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab5/er.php">Lab 5 aka er diagram</a></p>
+
+<p><a href="cs148_develop/labs/lab3/er-diagram.JPG">Lab 3 ER diagram</a></p>
+<p><a href="cs148_develop/labs/lab3/schema.html">Lab 3 Schema</a></p>
+<p><a href="cs148_develop/labs/lab3/data-dictionary.html">Lab 3 Data dictionary</a></p>
+<p><a href="https://ltrinity.w3.uvm.edu/cs148/cs148_develop/uvmclasses/tables.php">Tables.php</a></p>
+<p><a href="cs148_develop/gitworkflowtest.php">Git Workflow Test</a></p>
+<p>Test</p>
+</html>
