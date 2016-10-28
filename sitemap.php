095a04a9d94b7ce8b2099966a37095e717bdb4eb
Luke Trinity <ltrinity@uvm.edu>
2016-10-28 15:25:20 -0400

    style for sitemap

--- sitemap.php
+++ sitemap.php
@@ -8,22 +8,22 @@
         <link href="style.css" rel="stylesheet" type="text/css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-<p>Develop <a href="cs148_develop/labs/lab1/index.php">Lab1</a></p>
-<p>Develop <a href="cs148_develop/labs/lab2/index.php">Lab2</a></p>
-<p>Develop <a href="cs148_develop/labs/lab3/index.php">Lab3</a></p>
-<p>Develop <a href="cs148_develop/labs/lab4/index.php">Lab4</a></p>
-<p>Develop <a href="cs148_develop/labs/lab5/er.php">Lab5 aka er diagram</a></p>
+<p>Develop <a href="labs/lab1/index.php">Lab1</a></p>
+<p>Develop <a href="labs/lab2/index.php">Lab2</a></p>
+<p>Develop <a href="labs/lab3/index.php">Lab3</a></p>
+<p>Develop <a href="labs/lab4/index.php">Lab4</a></p>
+<p>Develop <a href="labs/lab5/er.php">Lab5 aka er diagram</a></p>
 
-<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab1/index.php">Lab1</a></p>
-<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab2/index.php">Lab2</a></p>
-<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab3/index.php">Lab3</a></p>
-<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab4/index.php">Lab4</a></p>
-<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/cs148live/labs/lab5/er.php">Lab 5 aka er diagram</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab1/index.php">Lab1</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab2/index.php">Lab2</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab3/index.php">Lab3</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab4/index.php">Lab4</a></p>
+<p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab5/er.php">Lab 5 aka er diagram</a></p>
 
-<p><a href="cs148_develop/labs/lab3/er-diagram.JPG">Lab 3 ER diagram</a></p>
-<p><a href="cs148_develop/labs/lab3/schema.html">Lab 3 Schema</a></p>
-<p><a href="cs148_develop/labs/lab3/data-dictionary.html">Lab 3 Data dictionary</a></p>
-<p><a href="https://ltrinity.w3.uvm.edu/cs148/cs148_develop/uvmclasses/tables.php">Tables.php</a></p>
-<p><a href="cs148_develop/gitworkflowtest.php">Git Workflow Test</a></p>
+<p><a href="labs/lab3/er-diagram.JPG">Lab 3 ER diagram</a></p>
+<p><a href="labs/lab3/schema.html">Lab 3 Schema</a></p>
+<p><a href="labs/lab3/data-dictionary.html">Lab 3 Data dictionary</a></p>
+<p><a href="uvmclasses/tables.php">Tables.php</a></p>
+<p><a href="gitworkflowtest.php">Git Workflow Test</a></p>
 <p>Test</p>
 </html>
