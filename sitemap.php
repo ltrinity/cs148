50e45c4ef118c917a6e2c510fcc10a2d70c13827
Luke Trinity <ltrinity@uvm.edu>
2016-11-11 14:55:35 -0500

    sitemap changes

--- sitemap.php
+++ sitemap.php
@@ -12,13 +12,16 @@
 <p>Develop <a href="labs/lab2/index.php">Lab2</a></p>
 <p>Develop <a href="labs/lab3/index.php">Lab3</a></p>
 <p>Develop <a href="labs/lab4/index.php">Lab4</a></p>
-<p>Develop <a href="labs/lab5/index.php">Lab5 index</a></p>
-<p>Develop <a href="labs/lab5/chooseCourse.php">Lab5 choose courses</a></p>
-<p>Develop <a href="labs/lab5/display.php">Lab5 display courses</a></p>
-<p>Develop <a href="labs/lab5/er.php">Lab5 er diagram</a></p>
-<p>Develop <a href="labs/lab5/schema.php">Lab5 schema</a></p>
-<p>Develop <a href="labs/lab5/dictionary.php">Lab5 data dictionary</a></p>
 
+<fieldset>
+    <legend>TA look here</legend>
+    <p><a href="labs/lab5/index.php">Lab5 index</a></p>
+<p><a href="labs/lab5/chooseCourse.php">Lab5 choose courses</a></p>
+<p><a href="labs/lab5/display.php">Lab5 display courses</a></p>
+<p><a href="labs/lab5/er.php">Lab5 er diagram</a></p>
+<p><a href="labs/lab5/schema.php">Lab5 schema</a></p>
+<p><a href="labs/lab5/dictionary.php">Lab5 data dictionary</a></p>
+</fieldset>
 <p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab1/index.php">Lab1</a></p>
 <p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab2/index.php">Lab2</a></p>
 <p>Live <a href="https://ltrinity.w3.uvm.edu/cs148/labs/lab3/index.php">Lab3</a></p>
