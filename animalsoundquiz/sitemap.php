04d0214c57fae2e36149391fb94c5eb797e6bc0e
Luke Trinity <ltrinity@uvm.edu>
2016-12-09 13:06:51 -0500

    about page

--- animalsoundquiz/sitemap.php
+++ animalsoundquiz/sitemap.php
@@ -1,6 +1,12 @@
 <?php
 //initialize database connection
 include "top.php";
+print '<fieldset class = "reviewquestions" >';
+print '<img alt="image" src="luke.jpg" class = "animal" id = "pleaseFloat">';
+print "<p class = 'large'>About</p>";
+print "<p class = 'moderate' id = 'pleaseFloat2'>Luke Trinity is a junior in the Honors College at the University of Vermont majoring in computer science. Originally from Washington, D.C., he loves the beautiful scenery and nature of Vermont. Since last summer, Luke has been employed by the Social Economic Gaming Simulation Lab. During the summer as part of the North East Water Resource Network, he created a game to study farmer's decision-making using Unity and C#. His current project is developing a compliance game to understand how pig industry workers weigh risk and reward.</p>";
+print '<strong><a href="http://www.uvm.edu/~segs/node/74">Click here to go to the SEGS website</a></strong>';
+print '</fieldset>';
 print '<fieldset class = "reviewquestions">';
 print '<p class = "large"><a href="git.pdf">Commits</a></p>';
 print '</fieldset>';
